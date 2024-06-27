495da64bddb56eec3db179ed162ac666ed4e67ab
wendigo390 <oscardaniel2101@gmail.com>
2024-06-27 08:13:28 -0600

    datos1 cambios

--- Huellitas1/datos1.cs
+++ Huellitas1/datos1.cs
@@ -13,5 +13,25 @@ namespace Huellitas1
             get { return _valorGlobal; }
             set { _valorGlobal = value; }
         }
+
+        public class usuarios
+        {
+            public string Nombres { get; set; }
+
+            public string Apellido { get; set; }
+
+            public string Password { get; set; }
+
+            public string IdRol { get; set; }
+
+            public string Correo { get; set; }
+
+            public string Id_Usuario { get; set; }
+
+            public string Nombre_Usuario { get; set; }
+
+
+
+        }
     }
 }
